6dbc7d9dae33e262b1db1ffdb7a72dcd6601e733
Pedro Henrique <pedrohenriquematiassantana@gmail.com>
2023-09-14 09:16:03 -0300

    Atualizando Repositorio Portfolio

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,11 +1,27 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
 
- function ativaLetra(eLemento){
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+
+function ativaLetra(eLemento){
     const arrTexto = eLemento.innerHTML.split('');
     eLemento.innerHTML = '';
     arrTexto.forEach((letra, i)=>{
         setTimeout(()=>{
             eLemento.innerHTML += letra; 
-        }, 75 * i);
+        }, 50 * i);
     });
  }
 
